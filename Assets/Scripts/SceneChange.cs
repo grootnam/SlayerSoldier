7a35d7bd2536a6c86923c3cd14276dcf238043f6
hjoannejiyeon <hjoanne@naver.com>
2020-11-28 17:29:23 +0900

    UI
    1. Main화면
    2. 스테이지 선택
    3. 스테이지 UI->SampleScene1에 있음

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Scene : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
     public void GoToStage1()
     {
--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -9,5 +9,69 @@ using System.IO;
 
 public class UImanager : MonoBehaviour
 {
-    
+    public bool isStageOver; // 스테이지 종료 여부
+
+    public Text Bullet;
+    public Text Qskill;
+    public Text Eskill;
+
+    GameObject Player;
+
+    public GameObject ResultPopup;
+
+    bool IsPause;
+
+    void Start()
+    {
+        ResultPopup.SetActive(false);
+        IsPause = false;
+    }
+
+    void Update()
+    {
+        //"T" -> 일시정지
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            if(IsPause==false)
+            {
+                Time.timeScale = 0;
+                IsPause = true;
+                return;
+            }
+            if(IsPause==true)
+            {
+                Time.timeScale = 1;
+                IsPause = false;
+                return;
+            }
+        }
+        //bullet개수
+        int leftBullet = GameObject.Find("Player").GetComponent<PlayerMove>().leftBulletNum;
+        Bullet.text = leftBullet.ToString();
+        
+        //Q, E 스킬
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Qskill.text = "<color=#ff0000>" + "Q" + "</color>";
+        }
+        else
+        {
+            Qskill.text = "<color=#ffffff>" + "Q" + "</color>";
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Eskill.text = "<color=#ff0000>" + "E" + "</color>";
+        }
+        else
+        {
+            Eskill.text = "<color=#ffffff>" + "E" + "</color>";
+        }
+    }
+
+    public void stageClear()
+    {
+        isStageOver = true;
+        ResultPopup.SetActive(true);
+    }
 }
