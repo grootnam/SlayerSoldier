e8b579f1053fc6ba99164836223648f410bd972f
G-Man-git <61372684+G-Man-git@users.noreply.github.com>
2020-12-10 03:56:17 +0900

    stage2 패턴 a,c 완성
    stage2 패턴 a,c 완성
    a) 유도 창
    b) 플레이어를 끌고와 때리기

---
+++ Assets/Scripts/Spear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer==LayerMask.NameToLayer("map")||other.gameObject.layer==LayerMask.NameToLayer("ground"))
+        {
+            gameObject.GetComponent<Rigidbody>().velocity=Vector3.zero;
+            Destroy(gameObject,2f);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.layer==LayerMask.NameToLayer("map")||other.gameObject.layer==LayerMask.NameToLayer("ground"))
+        {
+            gameObject.GetComponent<Rigidbody>().velocity=Vector3.zero;
+            Destroy(gameObject,2f);
+        }
+    }
+}
